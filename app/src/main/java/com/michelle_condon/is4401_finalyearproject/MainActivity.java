8b758b0956690acbeddf1b398236d245bfea9616
MichelleCondon <117320951@umail.ucc.ie>
2021-01-29 23:17:56 +0000

    Iteration 3- Splashscreen created, login page re-designed, commented and formatted

--- app/src/main/java/com/michelle_condon/is4401_finalyearproject/MainActivity.java
+++ app/src/main/java/com/michelle_condon/is4401_finalyearproject/MainActivity.java
@@ -1,17 +1,18 @@
 package com.michelle_condon.is4401_finalyearproject;
 
-//Import Statements
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -20,9 +21,10 @@ import com.google.firebase.auth.FirebaseAuth;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     //Code below is based on the Youtube Video Login and Registration Android App Tutorial using Firebase Authentication - Login, CodeWithMazn, https://www.youtube.com/watch?v=KB2BIm_m1Os&t=336s (1)
     //Code below is based on the website Firebase Documentation, Google Firebase, https://firebase.google.com/docs/auth/android/password-auth (2)
+
     //Declaring Variables
     private TextView register;
-    private EditText editTextEmail, editTextPassword;
+    private EditText txtEmail, txtPassword;
     private Button signIn;
     private FirebaseAuth mAuth;
 
@@ -31,88 +33,90 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Assigning values by resource Id's - Register button
-        register = (TextView) findViewById(R.id.register);
-        //Listening for the users button click for register
+        //Assigning values to variables with resource Id's - Register TextView
+        register = (TextView) findViewById(R.id.lblRegister);
+        //Listening for the users text view click for register
         register.setOnClickListener(this);
 
-        //Assigning values by resource Id's
-        signIn = (Button) findViewById(R.id.signIn);
-        //Listening for the users button click for register
+        //Assigning values to variables with resource Id's - Login Button
+        signIn = (Button) findViewById(R.id.btnLogin);
+        //Listening for the users button click for login
         signIn.setOnClickListener(this);
 
+        //Assigning values to variables with resource Id's - email and password text boxes
+        txtEmail = (EditText) findViewById(R.id.txtemail);
+        txtPassword = (EditText) findViewById(R.id.txtPassword);
 
-
-        //Assigning values by resource Id's - Login page email and password text fields
-        editTextEmail = (EditText) findViewById(R.id.txtemail);
-        editTextPassword = (EditText) findViewById(R.id.txtPassword);
-
-
+        //Entry point of Firebase authentication
         mAuth = FirebaseAuth.getInstance();
     }
     //End(2)
 
+    //OnClick Method
     @Override
     public void onClick(View v) {
-        //Java switch statement
+        //Switch Case statement
         switch (v.getId()) {
-            //Register Button
-            case R.id.register:
+            //If the register label is clicked the sign up screen opens
+            case R.id.lblRegister:
                 startActivity(new Intent(this, SignupScreen.class));
                 break;
-            //Sign in button
-            case R.id.signIn:
+            //If the login button is clicked the userLogin() method is called
+            case R.id.btnLogin:
                 userLogin();
                 break;
         }
     }
 
     private void userLogin() {
-        //Email and password strings
-        String email = editTextEmail.getText().toString().trim();
-        String password = editTextPassword.getText().toString().trim();
+        //Assigns email and password to their associated variable
+        String email = txtEmail.getText().toString().trim();
+        String password = txtPassword.getText().toString().trim();
+
         //Validation
-        //Ensuring Email field is filled
+        //Ensuring email field is filled
         if (email.isEmpty()) {
-            editTextEmail.setError("Email is required");
-            editTextEmail.requestFocus();
+            txtEmail.setError("Email Field is Required");
+            txtEmail.requestFocus();
             return;
         }
         //Ensuring email address is valid
         if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            editTextEmail.setError("Email is Incorrect, Please Check your Credentials");
-            editTextEmail.requestFocus();
+            txtEmail.setError("Invalid Email Address");
+            txtEmail.requestFocus();
             return;
         }
         //Ensuring password field is filled
         if (password.isEmpty()) {
-            editTextPassword.setError("Password is required");
-            editTextPassword.requestFocus();
+            txtPassword.setError("Password Field is required");
+            txtPassword.requestFocus();
             return;
         }
         //Ensuring the password is longer than 6 characters
         if (password.length() < 6) {
-            editTextPassword.setError("Password should be greater than 6 characters");
-            editTextPassword.requestFocus();
+            txtPassword.setError("Password needs to be longer than 6 characters");
+            txtPassword.requestFocus();
             return;
         }
+
         //Code below is based on the website Firebase Documentation, Google Firebase, https://firebase.google.com/docs/auth/android/password-auth (2)
-        //Authenticating sign in to Firebase using email and password parameters
+
+        //Authenticating sign in to Firebase using email and password parameters (Inbuilt function)
         mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-                //If the sign is a success
+                //If the login is successful the menu is opened
                 if (task.isSuccessful()) {
                     startActivity(new Intent(MainActivity.this, MainMenu.class));
 
                 } else {
-                    //If the sign in fails
-                    Toast.makeText(MainActivity.this, "Login Failed Please Check Your Credentials!", Toast.LENGTH_LONG).show();
+                    //If the login fails to authenticate an error message is displayed
+                    Toast.makeText(MainActivity.this, "Failed to Login, please check your credentials", Toast.LENGTH_LONG).show();
                 }
-                //End (1)
-                //End (2)
             }
         });
 
     }
 }
+//End (1)
+//End (2)
