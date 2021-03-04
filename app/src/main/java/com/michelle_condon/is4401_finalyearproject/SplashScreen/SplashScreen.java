97b5b339657081a68f8c031ee8d8bff9ffd74462
MichelleCondon <117320951@umail.ucc.ie>
2021-03-04 15:06:47 +0000

    Iteration 5 - Progress bar added to splash screen & new splash screen code implemented

---
+++ app/src/main/java/com/michelle_condon/is4401_finalyearproject/SplashScreen/SplashScreen.java
@@ -0,0 +1,57 @@
+package com.michelle_condon.is4401_finalyearproject.SplashScreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.michelle_condon.is4401_finalyearproject.MainActivity;
+import com.michelle_condon.is4401_finalyearproject.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    //Code below is based on the website "Android Splash Screen With Progress Bar Example" by Codexpedia, "https://www.codexpedia.com/android/android-splash-screen-with-progress-bar-example/"
+    //Logo was generated on https://hatchful.shopify.com/
+
+    //Declare progress bar as a variable
+    private ProgressBar mProgress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Show the splash screen
+        setContentView(R.layout.activity_splash_screen);
+        mProgress = (ProgressBar) findViewById(R.id.splashScreenProgressBar);
+
+        // Start lengthy operation in a background thread
+        new Thread(new Runnable() {
+            public void run() {
+                doWork();
+                startApp();
+                finish();
+            }
+        }).start();
+    }
+
+    private void doWork() {
+        //For loop to load progress bar on the screen
+        for (int progress = 10; progress < 100; progress += 10) {
+            try {
+                Thread.sleep(1000);
+                mProgress.setProgress(progress);
+            } catch (Exception e) {
+                Toast.makeText(SplashScreen.this, "App Failed to Load, Please Contact Management", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void startApp() {
+        //Open a new activity
+        Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
+//End
